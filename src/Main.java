2d51910af5980b65f88896998020615910e3bb7d
ozanmart <58523486+ozanmart@users.noreply.github.com>
2025-03-11 15:52:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        String whichMap = "15-15";
+        String whichMap = "20-20";
         String mapData = String.format("test_cases/%s/mapData.txt", whichMap);
         String mapDistanceData = String.format("test_cases/%s/distances.txt", whichMap);
         String objectiveData = String.format("test_cases/%s/objectives.txt", whichMap);
